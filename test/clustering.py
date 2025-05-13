d13f3979108cf7254770e10afd77285bf3c1fb7a
dqieu <tiendat691@gmail.com>
2025-05-13 10:40:06 -0400

    enforce emb shape

--- test/clustering.py
+++ test/clustering.py
@@ -4,10 +4,10 @@ from src import GreedyCohesiveClustering
 
 def test_clustering():
     nodes = [
-        Node(0,[0, 0]),
-             Node (1, [2, 0]),
-             Node (2, [0, 1]),
-             Node (3, [2, 1])
+        Node('0',[0, 0]),
+             Node ('1', [2, 0]),
+             Node ('2', [0, 1]),
+             Node ('3', [2, 1])
              ]
     graph = Graph(nodes, 2)
     print(GreedyCohesiveClustering(graph, graph.k))
