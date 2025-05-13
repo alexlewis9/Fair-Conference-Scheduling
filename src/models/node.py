fdd10462987fab5daa46fbeb6995387605495fe7
dqieu <tiendat691@gmail.com>
2025-05-13 08:55:57 -0400

    Node takes both id and emb now. - For usability - Logic is the same

--- src/models/node.py
+++ src/models/node.py
@@ -2,10 +2,12 @@ class Node:
     """
     Represents a generic node with an ID.
     """
-    def __init__(self, node_id):
+    def __init__(self, node_id, emb):
         """
         Initializes the node with a unique ID.
         Args:
             node_id (int): Unique identifier for the node.
+            emb (np.ndarray): Embedding (Coordinate) of the node.
         """
         self.id = node_id
+        self.emb = emb
--- test/clustering.py
+++ test/clustering.py
@@ -3,9 +3,13 @@ from src import GreedyCohesiveClustering
 
 
 def test_clustering():
-    nodes = [Node(i) for i in range(4)]
-    emb = [[0, 0], [2, 0], [0, 1], [2, 1]]
-    graph = Graph(nodes, emb, 2)
+    nodes = [
+        Node(0,[0, 0]),
+             Node (1, [2, 0]),
+             Node (2, [0, 1]),
+             Node (3, [2, 1])
+             ]
+    graph = Graph(nodes, 2)
     print(GreedyCohesiveClustering(graph, graph.k))
 
 
