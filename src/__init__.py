62b37230540cc56ad9eaa25a1544971e6446903d
dqieu <tiendat691@gmail.com>
2025-05-14 23:26:50 -0400

    avoiding double import when calling CLI (like in readme)

--- src/__init__.py
+++ src/__init__.py
@@ -4,6 +4,7 @@ from .models import PaperNode
 from .models import GreedyCohesiveClustering
 from .data_processing import process_pdfs
 from .data_processing import Encoder
+# to avoid runtime error of CLI call for double imports
 # from .data_processing import generate_embeddings
 # from .data_processing import csv_to_paper_node
 from .config import PROJECT_ROOT, DATA_DIR
--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -1,4 +1,5 @@
 from .pdfs_to_text import process_pdfs
 from .encoder import Encoder
+# to avoid runtime error of CLI call for double imports
 # from .csv_to_paper_node import main
 # from .generate_embeddings import main
