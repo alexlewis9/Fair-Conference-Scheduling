05a44cc4703a54f8da74cec0b9b5846fec2752c2
dqieu <tiendat691@gmail.com>
2025-05-14 23:19:30 -0400

    generate embeddings from json

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # config.py
 import os
 
-# Project root and data paths
+# Project root and data paths, for testing
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
--- src/data_processing/__init__.py
+++ src/data_processing/__init__.py
@@ -1,2 +1,4 @@
 from .pdfs_to_text import process_pdfs
 from .encoder import Encoder
+# from .csv_to_paper_node import main
+# from .generate_embeddings import main
