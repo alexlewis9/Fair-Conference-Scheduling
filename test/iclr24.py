37c64d6c8f1130ce075c32e3a24456a237abb044
Larry Kieu <tiendat691@gmail.com>
2025-05-28 11:41:59 -0400

    e2e ICLR2024 and misc.

--- test/iclr24.py
+++ test/iclr24.py
@@ -1,17 +1,32 @@
+import json
 import os
 
 import pandas as pd
-from src import DATA_DIR
+from openai import embeddings
+
+from src import DATA_DIR, generate_embeddings, Node, Graph, GreedyCohesiveClustering
 
 
-def test_iclr24():
-    metadata_file = os.path.join(DATA_DIR, 'metadata', 'ICLR_2021_2024.csv')
-    # Load the CSV file
-    df = pd.read_csv('your_file.csv')
+def test_iclr24_abstract_basic():
+    input_path = os.path.join(DATA_DIR, 'unified_text', 'ICLR', f'ICLR_2024.json')
+    output_path = os.path.join(DATA_DIR, 'emb', 'ICLR')
+    model = 'text-embedding-3-large'
+    inclusion = ['abstract', 'title', 'authors']
 
-    # Filter rows where the 'year' column is 2024
-    filtered_ids = df[df['year'] == 2024]['id']
+    embeddings = generate_embeddings(input_path, output_path, model, include=inclusion)
 
-    # Convert to list (optional)
-    id_list = filtered_ids.tolist()
+    nodes = []
+    for key, value in embeddings.items():
+        nodes.append(Node(key, value))
+
+    graph = Graph(nodes, 32)
+    print(GreedyCohesiveClustering(graph, 32))
+
+
+if __name__ == '__main__':
+    test_iclr24_abstract_basic()
+
+
+
+
 
