37157e02178b273721e504bf438ffbffa567f75b
Larry Kieu <tiendat691@gmail.com>
2025-05-29 12:33:08 -0400

    load original cluster

---
+++ src/data_processing/load_cluster.py
@@ -0,0 +1,15 @@
+# TODO: save and load clusters
+import json
+
+
+def load_cluster_from_data(path, to_dict=False):
+    with open(path, 'r') as f:
+        file = json.load(f)
+    clustering_og = {}
+    for entry in file:
+        if entry['session'] not in clustering_og:
+            clustering_og[entry['session']] = [entry['id']]
+        else:
+            clustering_og[entry['session']].append(entry['id'])
+
+    return clustering_og if to_dict else [value for _, value in clustering_og.items()]
