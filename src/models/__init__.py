11d14af64b135f1b1711d94f1062e0ba284d4f73
Larry Kieu <tiendat691@gmail.com>
2025-05-30 18:11:43 -0400

    clean house

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from .node import Node
 from .paper_node import PaperNode
 from .graph import Graph
-from .clustering import GreedyCohesiveClustering
+from .clustering import greedy_cohesive_clustering
